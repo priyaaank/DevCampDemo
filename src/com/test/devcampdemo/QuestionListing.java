25683652b86731530d6cd355a8177ebca2103e9e
Priyank <priyaaank@gmail.com>
2011-04-06 21:56:01 +0530

    Invoked map intent with right params. End of app dev.

--- src/com/test/devcampdemo/QuestionListing.java
+++ src/com/test/devcampdemo/QuestionListing.java
@@ -5,6 +5,7 @@ import java.util.List;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
@@ -24,6 +25,7 @@ public class QuestionListing extends ListActivity {
 	private PuneDemoDatabase database;
 	private QuestionTable reviewTable;
 	private ArrayAdapter<String> messageListAdapter = null;
+	private List<Question> questions;
 	
 	private final static int QUIT = 1;
 	
@@ -53,6 +55,7 @@ public class QuestionListing extends ListActivity {
 	
 	private void updateListingContentsWith(List<Question> questions) {
 		if(questions != null  && questions.size() > 0) {
+			this.questions = questions;
 			String[] questionTitles = new String[questions.size()];
 			int index = 0;
 			for(Question eachQuestion : questions) {
@@ -65,12 +68,10 @@ public class QuestionListing extends ListActivity {
 	
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-//		if(reviewList != null && reviewList.size() >= position) {
-//			Log.i(LOG_TAG, "List item clicked at position :: " + position);
-//			Intent intent = new Intent(this, ReviewDetailsActivity.class);
-//			intent.putExtra("REVIEW_ID", reviewList.get(position).getId());
-//			startActivity(intent);
-//		}
+		if(questions != null && questions.size() >= position) {
+			String uri = "geo:"+ questions.get(position).getLatitude() +","+ questions.get(position).getLongitude() +"?z=15";
+			startActivity(new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri)));
+		}
 	}
 	
 	@Override
